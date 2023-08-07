c7cbdc281eb3733264eade7d60db56b44e27701c
Ogrnz <olivier.grognuz@gmail.com>
2023-08-07 23:26:59 +0200

    tests: add more tests for helpers

--- tests/test_helpers.py
+++ tests/test_helpers.py
@@ -1,4 +1,4 @@
-from unittest import TestCase
+import unittest
 
 import numpy as np
 import numpy.testing as npt
@@ -6,7 +6,7 @@ import numpy.testing as npt
 from feval import helpers
 
 
-class Test(TestCase):
+class Test(unittest.TestCase):
     f0 = None
     f1 = None
     F = None
@@ -28,11 +28,42 @@ class Test(TestCase):
         L = helpers.ape(self.y, self.F)
         epsilon = np.finfo(np.float64).eps
 
-        npt.assert_array_equal(L[:, 0], np.abs(self.y - self.f0) / np.maximum(np.abs(self.y), epsilon))
-        npt.assert_array_equal(L[:, 1], np.abs(self.y - self.f1) / np.maximum(np.abs(self.y), epsilon))
+        npt.assert_array_equal(
+            L[:, 0], np.abs(self.y - self.f0) / np.maximum(np.abs(self.y), epsilon)
+        )
+        npt.assert_array_equal(
+            L[:, 1], np.abs(self.y - self.f1) / np.maximum(np.abs(self.y), epsilon)
+        )
 
     def test_se(self):
         L = helpers.se(self.y, self.F)
 
         npt.assert_array_equal(L[:, 0], (self.y - self.f0) ** 2)
         npt.assert_array_equal(L[:, 1], (self.y - self.f1) ** 2)
+
+    def test_ae_shape_mismatch(self):
+        with self.assertRaises(ValueError):
+            helpers.ae(np.array([1, 2]), np.array([[1, 2], [3, 4], [5, 6]]))
+
+    def test_ape_zero_y_true(self):
+        y = np.array([0, 0, 0, 0])
+        F = np.array([[1, 2], [3, 4], [5, 6], [7, 8]])
+        L = helpers.ape(y, F)
+        self.assertTrue(np.all(L[:, 0] > 1e10))  # Check if all values are big indeed
+
+    def test_vec_to_col(self):
+        vec = np.array([1, 2, 3])
+        col_vec = helpers.vec_to_col(vec)
+        self.assertEqual(col_vec.shape, (3, 1))
+        npt.assert_array_equal(col_vec, np.array([[1], [2], [3]]))
+
+    def test_ae_with_list(self):
+        y = [0, 0, 0, 0]
+        F = [[1, 2], [3, 4], [5, 6], [7, 8]]
+
+        with self.assertRaises(TypeError):
+            helpers.ae(y, F)
+
+
+if __name__ == "__main__":
+    unittest.main()
